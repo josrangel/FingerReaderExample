5f024051b700ae317b318da78b2862fdbc87e33f
Josue <josrangel@hotmail.com>
2021-08-30 02:09:15 -0500

    - add implementation

--- app/src/main/java/com/jrangel/fingerreaderexample/MainActivity.kt
+++ app/src/main/java/com/jrangel/fingerreaderexample/MainActivity.kt
@@ -1,11 +1,178 @@
 package com.jrangel.fingerreaderexample
 
+import android.Manifest
+import android.annotation.TargetApi
+import android.app.KeyguardManager
+import android.content.pm.PackageManager
+import android.hardware.fingerprint.FingerprintManager
+import android.os.Build
 import android.os.Bundle
+import android.security.keystore.KeyGenParameterSpec
+import android.security.keystore.KeyPermanentlyInvalidatedException
+import android.security.keystore.KeyProperties
+import android.widget.TextView
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import java.io.IOException
+import java.security.*
+import java.security.cert.CertificateException
+import javax.crypto.Cipher
+import javax.crypto.KeyGenerator
+import javax.crypto.NoSuchPaddingException
+import javax.crypto.SecretKey
 
-class MainActivity : AppCompatActivity() {
+/**
+ * Tutorial
+ * https://baturamobile.com/blog/identificacion-biometrica-con-huella-dactilar-movil/
+ */
+
+class MainActivity : AppCompatActivity(), OnUpdateListener {
+
+    lateinit private var tvErrorText: TextView
+    lateinit private var keyStore: KeyStore
+
+    // Variable used for storing the key in the Android Keystore container
+    private val KEY_NAME = "pruebaHuella"
+    lateinit private var cipher: Cipher
+
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        tvErrorText = findViewById(R.id.tv_error_text)
+        initFingerService()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    private fun initFingerService() {
+        val keyguardManager = getSystemService(KEYGUARD_SERVICE) as KeyguardManager
+        val fingerprintManager = getSystemService(FINGERPRINT_SERVICE) as FingerprintManager
+
+        // Check whether the device has a Fingerprint sensor.
+        if (!fingerprintManager.isHardwareDetected()) {
+            /**
+             * An error message will be displayed if the device does not contain the fingerprint hardware.
+             * However if you plan to implement a default authentication method,
+             * you can redirect the user to a default authentication activity from here.
+             * Example:
+             * Intent intent = new Intent(this, DefaultAuthenticationActivity.class);
+             * startActivity(intent);
+             */
+            tvErrorText.text = "Your Device does not have a Fingerprint Sensor"
+        } else {
+            // Checks whether fingerprint permission is set on manifest
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.USE_FINGERPRINT
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                tvErrorText.text = "Fingerprint authentication permission not enabled"
+            } else {
+                // Check whether at least one fingerprint is registered
+                if (!fingerprintManager.hasEnrolledFingerprints()) {
+                    tvErrorText.text = "Register at least one fingerprint in Settings"
+                } else {
+                    // Checks whether lock screen security is enabled or not
+                    if (!keyguardManager.isKeyguardSecure()) {
+                        tvErrorText.text = "Lock screen security not enabled in Settings"
+                    } else {
+                        generateKey()
+                        if (cipherInit()) {
+                            val cryptoObject = FingerprintManager.CryptoObject(
+                                cipher!!
+                            )
+                            val helper = FingerprintHandler(this, this)
+                            helper.startAuth(fingerprintManager, cryptoObject)
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.M)
+    protected fun generateKey() {
+        try {
+            keyStore = KeyStore.getInstance("AndroidKeyStore")
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        val keyGenerator: KeyGenerator
+        keyGenerator = try {
+            KeyGenerator.getInstance(KeyProperties.KEY_ALGORITHM_AES, "AndroidKeyStore")
+        } catch (e: NoSuchAlgorithmException) {
+            throw RuntimeException("Failed to get KeyGenerator instance", e)
+        } catch (e: NoSuchProviderException) {
+            throw RuntimeException("Failed to get KeyGenerator instance", e)
+        }
+
+        try {
+            keyStore.load(null)
+            keyGenerator.init(
+                KeyGenParameterSpec.Builder(
+                    KEY_NAME,
+                    KeyProperties.PURPOSE_ENCRYPT or
+                            KeyProperties.PURPOSE_DECRYPT
+                )
+                    .setBlockModes(KeyProperties.BLOCK_MODE_CBC)
+                    .setUserAuthenticationRequired(true)
+                    .setEncryptionPaddings(
+                        KeyProperties.ENCRYPTION_PADDING_PKCS7
+                    )
+                    .build()
+            )
+            keyGenerator.generateKey()
+        } catch (e: NoSuchAlgorithmException) {
+            throw RuntimeException(e)
+        } catch (e: InvalidAlgorithmParameterException) {
+            throw RuntimeException(e)
+        } catch (e: CertificateException) {
+            throw RuntimeException(e)
+        } catch (e: IOException) {
+            throw RuntimeException(e)
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.M)
+    fun cipherInit(): Boolean {
+        cipher = try {
+            Cipher.getInstance(KeyProperties.KEY_ALGORITHM_AES + "/" + KeyProperties.BLOCK_MODE_CBC + "/" + KeyProperties.ENCRYPTION_PADDING_PKCS7)
+        } catch (e: NoSuchAlgorithmException) {
+            throw RuntimeException("Failed to get Cipher", e)
+        } catch (e: NoSuchPaddingException) {
+            throw RuntimeException("Failed to get Cipher", e)
+        }
+        return try {
+            keyStore.load(null)
+            val key = keyStore.getKey(
+                KEY_NAME,
+                null
+            ) as SecretKey
+            cipher.init(Cipher.ENCRYPT_MODE, key)
+            true
+        } catch (e: KeyPermanentlyInvalidatedException) {
+            false
+        } catch (e: KeyStoreException) {
+            throw RuntimeException("Failed to init Cipher", e)
+        } catch (e: CertificateException) {
+            throw RuntimeException("Failed to init Cipher", e)
+        } catch (e: UnrecoverableKeyException) {
+            throw RuntimeException("Failed to init Cipher", e)
+        } catch (e: IOException) {
+            throw RuntimeException("Failed to init Cipher", e)
+        } catch (e: NoSuchAlgorithmException) {
+            throw RuntimeException("Failed to init Cipher", e)
+        } catch (e: InvalidKeyException) {
+            throw RuntimeException("Failed to init Cipher", e)
+        }
+    }
+
+    override fun update(msg: String, success: Boolean) {
+        tvErrorText.text = msg
+        if (success) {
+            tvErrorText.setTextColor(ContextCompat.getColor(this, R.color.successText))
+        }
     }
 }
